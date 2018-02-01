0ee170c585038667d0505d00d93f08af6212d646
Giao Nguyen <giao.nguyenn@yahoo.com>
2018-02-01 14:31:02 -0600

    got props to pass to individual article

---
+++ src/components/Articles/Article/Summary/Summary.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import "./Summary.css";
+
+export default class Summary extends Component {
+	render() {
+		return (
+			<div className="summary">
+				<p>Modern UI teams are designing components first; Interfaces are merely the thoughtful composition of components. This leaves an often glaring hole for users on “the unhappy path” — The places where users may, intentionally or not, stray from your idealized flow. As we learn to craft systems rather than pages, we must invest effort into shaping these often missed states of design and create with a component lifecycle that can support everyone. Here’s the lifecycle as I see it:
+				</p> 
+			</div>
+		)
+	}
+}
